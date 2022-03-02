2bf284528b0c7bf2a95b43b0e4d521eb0c3626e3
Huy Quach <huy.quach@snyk.io>
2022-03-01 21:02:01 -0800

    added html for the code highlighting

--- src/lib/codeutil.ts
+++ src/lib/codeutil.ts
@@ -6,27 +6,43 @@ const nReadlines = require('n-readlines');
 type codeSource = {
     codelineno: number
     codesource: string
+    codesourcehtml: string
 };
 
-function readCodeLines(filePath, lines){
+function stringToHtml(pre: string, highlight: string, post: string){
+    const highlightrule = `${pre}<span class="marker">${highlight}</span>${post}`;
+    return highlightrule;
+}
+
+function readCodeLines(filePath, region){
     try{
-        const endLine = lines.endLine;
-        const startLine = (lines.startLine == endLine) ? endLine-4: lines.startLine ;
+        const endLine = region.endLine;
+        const startLine = (region.startLine == endLine) ? endLine-4: region.startLine ;
         const codeString: Array<codeSource> = [];
         const readSourceFile = new nReadlines(filePath);
         
         let startRecording=false;
         let line;
         let lineNumber = 1;
+        let parseline = "";
 
         while (line = readSourceFile.next()) {
 
             if ( lineNumber == startLine ) startRecording = true;
-            if ( lineNumber > endLine ) break;
+            else if ( lineNumber == endLine ) {
+                parseline = line.toString('ascii');
+                parseline = stringToHtml(
+                    parseline.substring(0, region.startColumn-1),
+                    parseline.substring(region.startColumn-1, region.endColumn),
+                    parseline.substring(region.endColumn, parseline.length)
+                );
+            }
+            else if ( lineNumber > endLine ) break;
+            
             if (startRecording){
                 //source.codelineno = lineNumber;
                 //source.codesource = `${line}`;
-                codeString.push({codelineno: lineNumber, codesource:`${line}`});
+                codeString.push({codelineno: lineNumber, codesource: `${line}`, codesourcehtml: parseline});
             }
             lineNumber++;
         }
